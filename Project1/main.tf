8333eb7685c2c1d78adcb1abe6b0f410c7ae4ff6
Shyamprasad <admin@Admins-MacBook-Pro.local>
2025-03-09 08:28:56 +0530

    adding .terraform

---
+++ Project1/main.tf
@@ -0,0 +1,12 @@
+# provider "aws" {
+#   region = "ap-south-1"
+  
+# }
+
+resource "aws_instance" "State_file_demo" {
+  ami = "ami-05c179eced2eb9b5b"
+  instance_type = "t2.micro"
+    tags = {
+        Name = "State_file_demo1"
+    }
+}
