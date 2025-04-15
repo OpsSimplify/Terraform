506093fcb2d3e2f236d4f4fcc1c9e69581202549
Shyamprasad reddy <shyam808ganji@gmail.com>
2025-04-15 09:33:53 +0530

    Update main.tf

--- project2/main.tf
+++ project2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform-demo" {
     ami = var.ami
     instance_type = var.instance_type
     tags = {
-        Name = "terraform-demo"
+        Name = "terraform-dem"
         env = "test"
     }
 }
